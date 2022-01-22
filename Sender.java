47e1e1d06a49590a5c5952b24de873b20cc6f961
Mounika526-17 <84308900+Mounika526-17@users.noreply.github.com>
2022-01-22 23:45:17 +0530

    Update Sender.java

--- Sender.java
+++ Sender.java
@@ -1,5 +1,19 @@
-package multithreading;
-
-public class Sender {
-
-}
+package ThreadSynchronization;
+import java.io.*; 
+import java.util.*; 
+class Sender 
+{ 
+    public void send(String msg) 
+    { 
+        System.out.println("Sending\t"  + msg ); 
+        try
+        { 
+            Thread.sleep(1000); 
+        } 
+        catch (Exception e) 
+        { 
+            System.out.println("Thread  interrupted."); 
+        } 
+        System.out.println("\n" + msg + "Sent"); 
+    } 
+} 
